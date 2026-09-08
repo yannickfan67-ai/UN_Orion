09fd55556cc22c60617276e60c5b691c3636a388
yannickfan67-ai <yannickfan67@gmail.com>
2026-09-09 05:14:27 +0800

    Add central Orion version contract

---
+++ include/version.h
@@ -0,0 +1,12 @@
+#ifndef ORION_VERSION_H
+#define ORION_VERSION_H
+
+#define ORION_VERSION "0.0.7"
+
+#ifndef ORION_ARCH_NAME
+#define ORION_ARCH_NAME "unknown"
+#endif
+
+#define ORION_VERSION_STRING "UN_Orion " ORION_VERSION
+
+#endif
