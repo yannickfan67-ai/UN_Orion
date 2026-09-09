a76176d6eadeb2d8fe6b3d4df3b400ce3f64deab
yannickfan67-ai <yannickfan67@gmail.com>
2026-09-09 21:43:32 +0800

    input: expose PS/2 mouse packet size for wheel support

--- include/interrupts.h
+++ include/interrupts.h
@@ -8,5 +8,6 @@ uint32_t timer_frequency(void);
 int keyboard_pop_scancode(uint8_t *scan);
 int mouse_pop_byte(uint8_t *byte);
 int mouse_available(void);
+int mouse_packet_size(void);
 __attribute__((noreturn)) void orion_exception_panic(uint64_t vector,uint64_t error,uint64_t rip,uint64_t cs,uint64_t rflags);
 #endif
